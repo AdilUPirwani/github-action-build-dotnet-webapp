6f2cc5f269510bf966698b81cb5accd50daf0642
AdilWK <132672604+AdilWK@users.noreply.github.com>
2023-12-07 15:46:25 +0530

    Add project files.

---
+++ WebAppFramework.Unit/Controllers/HomeControllerTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Web.Mvc;
+using WebAppFramework.Controllers;
+
+namespace WebAppFramework.Unit.Controllers
+{
+    [TestClass]
+    public class HomeControllerTest
+    {
+        [TestMethod]
+        public void Index_Success()
+        {
+            HomeController controller = new HomeController();
+            var result = controller.Index();
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(result.GetType(), typeof(ViewResult));
+        }
+    }
+}
---
+++ WebAppFramework.Unit/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("WebAppFramework.Unit")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Wolters Kluwer")]
+[assembly: AssemblyProduct("WebAppFramework.Unit")]
+[assembly: AssemblyCopyright("Copyright © Wolters Kluwer 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("221ab704-655d-4747-b506-dd7dd014daa8")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
